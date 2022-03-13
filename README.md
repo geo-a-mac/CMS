9bab302152ba6386f9d10a1614fa4421aeae61f8
George Mcalpine <georgemcalpine@Georges-iMac.local>
2022-03-13 11:53:33 -0400

    Update employee

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # CMS
+Command line application that allows users to view:
+1. Department table
+2. Role table
+3. Employee table
+
+User can also:
+1. Add role
+2. Add employee
+3. Update employee role
+
+## Technology
+- Javascript
+- MySql2
+- npm package: console-table
+- npm package: 
+
+## Instructions
+At the command line run: node server.js
+Make selection from list using arrow keys:
+? What would you like to do? (Use arrow keys)
+❯ View all departments 
+  View all roles 
+  View all employees 
+  Add a department 
+  Add a role 
+  Add an employee 
+  Update an employee role
+
+Follow prompts.
--- src/prompts.js
+++ src/prompts.js
@@ -14,8 +14,7 @@ function promptUser() {
                         "Add a department",
                         "Add a role",
                         "Add an employee",
-                        "Update an employee role",
-                        "Exit"
+                        "Update an employee role"
                     ]
         }
     ])
@@ -47,45 +46,168 @@ function nextAction(answer) {
         .catch(console.log)
         .then(()=> db.end());
     } else if(answer.userChoice === "Add a department") {
-        console.log('add dept picked');
+        addDept();
         return;
     } else if(answer.userChoice === "Add a role") {
-        console.log('add role picked');
+        addRole();
         return;
     } else if(answer.userChoice === "Add an employee") {
-        console.log('add employee picked');
+        addEmployee();
         return;
     } else if(answer.userChoice === "Update an employee role") {
-        console.log('update role picked');
-        return;
-    } else if(answer.userChoice === "Exit") {
-        console.log('Exit picked');
+        updateEmployee();
         return;
     } 
 }
 
 function addDept() {
     return inquirer.prompt([
-
+        {
+            type: "input",
+            name: "department_name",
+            message: "Enter department name",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter department name");
+                }
+            }
+        }
     ])
+    .then(answer => {
+        const sql = `INSERT INTO department (department_name) VALUES ('${answer.department_name}')`;
+        db.promise().query(sql)
+        .catch(console.log)
+        .then(()=>db.end());
+    })
 }
 
 function addRole() {
     return inquirer.prompt([
-        
+        {
+            type: "input",
+            name: "title",
+            message: "Enter role title",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter role");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "Enter salary",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please salary");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "department_id",
+            message: "Enter department ID",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please department ID");
+                }
+            }
+        }
     ])
+    .then(answer => {
+        const sql = `INSERT INTO role (title, salary, department_id) VALUES ('${answer.title}','${answer.salary}','${answer.department_id}')`;
+        db.promise().query(sql)
+        .catch(console.log)
+        .then(()=>db.end());
+    })
 }
 
 function addEmployee() {
     return inquirer.prompt([
-        
+        {
+            type: "input",
+            name: "first_name",
+            message: "Enter first name",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter first name");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message: "Enter last name",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter last name");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "role_id",
+            message: "Enter role ID",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter Role ID");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "manager_id",
+            message: "Enter manager ID",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter manager ID");
+                }
+            }
+        }
     ])
+    .then(answer => {
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answer.first_name}','${answer.last_name}','${answer.role_id}','${answer.manager_id}')`;
+        db.promise().query(sql)
+        .catch(console.log)
+        .then(()=>db.end());
+    })
 }
 
 function updateEmployee() {
     return inquirer.prompt([
-        
+        {
+            type: "input",
+            name: "employee_id",
+            message: "Enter employee id",
+        },
+        {
+            type: "input",
+            name: "role",
+            message: "Enter updated role",
+        }
     ])
+    .then(answer => {
+        const sql = `UPDATE employee SET role_id=${answer.role} WHERE id = ${answer.employee_id}`;
+        db.promise().query(sql)
+        .catch(console.log)
+        .then(()=>db.end());
+    })
 }
 
 module.exports = {
